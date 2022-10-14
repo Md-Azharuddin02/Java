8f7e1ed831cbd0bf2339875d7b211f05e9d4217a
Md-Azharuddin02 <mdazharuddin155332@gmail.com>
2022-10-14 11:47:31 -0700

    commit

--- OOPS/ClassAndObjects.java
+++ OOPS/ClassAndObjects.java
@@ -7,12 +7,22 @@ class ByReferingVariable{
     int id;
     String username="";
 }
+class InitializingThrowTheMethod{
+    int id2;
+    String name2="";
+    void show(int id2,String name2){
+        this.id2=id2;
+        this.name2=name2;
+    }
+}
 public class ClassAndObjects {
     public static void main(String[] args) {
         ByReferingVariable b= new ByReferingVariable();
         b.id=11192732;
-        b.username="Prince sainger";
-        System.out.println(b.id);
-        System.out.println(b.username);
+        b.username="Prince sainger";     
+        System.out.println("by refering variables "+b.id);          
+        System.out.println("by refering variable "+b.username);
+
+        
     }
 }

c6ffba3f086ddd5f518d839e847476928dd52f91
Md-Azharuddin02 <mdazharuddin155332@gmail.com>
2022-10-14 11:38:52 -0700

    commit

--- OOPS/ClassAndObjects.java
+++ OOPS/ClassAndObjects.java
@@ -1,7 +1,18 @@
 package OOPS;
+ 
+// There are three ways to Initialize an objects
 
+// 1---------------------By refering Variable-----------------/
+class ByReferingVariable{
+    int id;
+    String username="";
+}
 public class ClassAndObjects {
     public static void main(String[] args) {
-        System.out.println("hello class and objects");
+        ByReferingVariable b= new ByReferingVariable();
+        b.id=11192732;
+        b.username="Prince sainger";
+        System.out.println(b.id);
+        System.out.println(b.username);
     }
 }

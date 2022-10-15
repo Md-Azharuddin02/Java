3fb40d1f879b336b9853b5a6dc96c3a651e4594b
Md-Azharuddin02 <mdazharuddin155332@gmail.com>
2022-10-15 12:16:14 -0700

    commit

--- OOPS/Inheritence.java
+++ OOPS/Inheritence.java
@@ -1,7 +1,65 @@
 package OOPS;
 
+// ---------------------Single Inheritence----------------
+class One{
+    void show(){
+        System.out.println("This is the parent classs method");
+    }
+}
+class Two extends One{
+    void showagain(){
+        System.out.println("This is the secound class method ");
+    }
+}
+
+
+// -------------------Multilevel Inheritence----------------------
+class Three extends Two{
+    void showThree(){
+        System.out.println("This is three class method");
+    }
+}
+
+// -----------------------Hirarichal Inheritence-------------------
+class A extends One{
+    void showA(){
+        System.out.println("This is A class method");
+}
+}
+class B extends One{
+    void showB(){
+        System.out.println("This is B class method");
+    }
+}
+class C extends One{
+    void showC(){
+        System.out.println("This C class method ");
+    }
+}
 public class Inheritence {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // Two t= new Two();          // This is single Inheritence
+        // t.show();                 //This is single Inheritence
+        // t.showagain();            //This is single Inheritence
+
+        // -----------------------------------------------------
+        //Three t=new Three();          //This is multilevel Inheritence       
+        //t.show();                    //This is multilevel Inheritence 
+       // t.showagain();               //This is multilevel Inheritence 
+        //t.showThree();               //This is multilevel Inheritence 
+        // ---------------------------------------------------------
+        A a = new A();              // Hirarichal Inheritence
+        a.show();
+        a.showA();
+
+        B b= new B();               // Hirarichal Inheritence
+        b.show();
+        b.showB();
+
+        C c= new C();
+        c.show();                   // Hirarichal Inheritence
+        c.showC();
+
+
     }
 }
